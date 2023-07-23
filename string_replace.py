7468dcf38bb812e9a9d87b71763598f75b5cf7ce
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2023-07-23 19:44:43 +0530

    sound_program

---
+++ string_replace.py
@@ -0,0 +1,14 @@
+text = '''Dear name,
+        you are selected!
+        in porn industry
+        you have such big dick of size inches
+        '''
+
+
+name = input("Enter your name:- ")
+size = input("Enter size:- ")
+
+text = text.replace("name",name)
+text = text.replace("size",size)
+
+print(text)
