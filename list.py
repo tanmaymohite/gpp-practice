fe60bcfa7a27b3edcf7a4265173eee865a825299
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2023-07-23 20:30:49 +0530

    sound_program

---
+++ list.py
@@ -0,0 +1,3 @@
+mf=["jordi","sexy maxy","sai",69]
+print(mf)
+print(mf[0:2])
---
+++ listsequence.py
@@ -0,0 +1,19 @@
+list1=[5,65,12,48,65,95,78,56,32]
+print(list1)
+
+list1.sort() #arrange in assending order
+#print(list1)
+list1.reverse()#arrange in reverse
+#print(list1)
+list1.append(69)# add 69 in the end
+#print(list1)
+
+list1.insert(65,66)
+print(list1)
+
+list1.pop(5)#remove element at index 5
+#print(list1)
+
+list1.remove(69)#to remove the element
+#print(list1)
+
